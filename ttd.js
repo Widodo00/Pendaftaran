703bbf707ba0927e3ca1844df716513354f28d75
Widodo00 <77629007+Widodo00@users.noreply.github.com>
2023-01-12 14:43:30 +0700

    Update ttd.js

--- ttd.js
+++ ttd.js
@@ -12,7 +12,7 @@ async function getDatattd() {
       return {
         KTP: values[1].formattedValue,
         nama: values[2].formattedValue,
-        tanggalttd: values[16].formattedValue,
+        tanggalttd: values[22].formattedValue,
         fotottd: String(values[15].formattedValue).replaceAll("open", "uc"),
       };
     });
